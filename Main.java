ac330b4d3d531e12ab8e890ac78a574f7f0c8ec9
anjunthomas <124937896+anjunthomas@users.noreply.github.com>
2023-08-12 23:45:11 -0400

    Learned interface implementation for Multithreading

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args){
         for(int i = 0; i < 5; i++){
-            MultithreadThing myThing = new MultithreadThing();
+            MultithreadThing myThing = new MultithreadThing(i);
             myThing.start();
+            System.out.println(myThing.isAlive()); // prints whether thread is alive or not, boolean value produced
         }
         /*
         MultithreadThing myThing = new MultithreadThing();
--- MultithreadThing.java
+++ MultithreadThing.java
@@ -1,10 +1,20 @@
 public class MultithreadThing extends Thread {
+    /* or public class MultiThreadTing implements Runnable*/
+    // The benefit of implementing an interface is that you
+    // can implement as many interfaces as you want
+    // however, you cannot extend more than one class
+    // so implement as interface so you can extend another class
+    // if you need to.
+    private int threadNumber;
+    public MultithreadThing(int threadNumber){
+        this.threadNumber = threadNumber;
+    }
     @Override
     public void run(){ // run method can contain any code, but must be public
                         // take no arguments, have no return value, and
                         // throw no exceptions
         for(int i = 1; i <=  5; i++){
-            System.out.println(i);
+            System.out.println(i + "from thread" + threadNumber);
 
             try {
                 Thread.sleep(1000);
